89f294439b669d8ec5267555b67cadf8de3320aa
“csureshkumargit” <csureshkumargce@gmail.com>
2021-10-25 03:11:19 -0400

    Initial Checkin

--- Router/Users.js
+++ Router/Users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const usersController = require('../Controller/Users');
+const usersController = require('../Controller/users');
 
 
 router.post('/Register', usersController.UserRegistartion);
