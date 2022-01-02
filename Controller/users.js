61c3e2f6e1feeefff3953094ba82a077b71b0fa1
“csureshkumargit” <csureshkumargce@gmail.com>
2022-01-02 04:21:06 -0500

    cookies for authentication

--- Controller/users.js
+++ Controller/users.js
@@ -64,7 +64,7 @@ exports.userLogin = async (req, res) => {
         const token = jwt.sign({ userNameFound: emailFound.username }, process.env.CLIENT_KEY, { expiresIn: '1h' });
         res.cookie('jwt_token', token, {
             maxAge: 1000 * 60 * 60 * 1,
-            secure: false,
+            secure: true,
             httpOnly: true,
             // withCredentials: true,
             // credentials: "include"
