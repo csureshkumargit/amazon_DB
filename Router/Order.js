0436843897bfcb495031e9f410dbad1d0bb0f97b
“csureshkumargit” <csureshkumargce@gmail.com>
2022-01-01 23:10:27 -0500

    updating package-lock file

--- Router/Order.js
+++ Router/Order.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const route = express.Router();
 const ordersController = require('../Controller/order');
-const usersController = require('../Controller/Users');
+const usersController = require('../Controller/users');
 route.get('/:username', usersController.validateToken, ordersController.getUserOrderHistory);
 route.post('/', ordersController.addUserOrder);
 
