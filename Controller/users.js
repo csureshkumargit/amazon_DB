a10a8d9cbc022b8877ca679ca8f797ff7deb23f8
“csureshkumargit” <csureshkumargce@gmail.com>
2022-01-03 05:14:27 -0500

    cookies for authentication

--- Controller/users.js
+++ Controller/users.js
@@ -66,7 +66,7 @@ exports.userLogin = async (req, res) => {
             maxAge: 1000 * 60 * 60 * 1,
             secure: true,
             httpOnly: true,
-            SameSite: 'none'
+            SameSite: 'None'
             // credentials: "include"
         });
         return res.header('access-token', token).send({ jwt: token, message: "You have been Logged in Successfully", username: emailFound.username, isAuthenticated: true });
