3de298e372cffd2f3a4c3ad4dfed201f87496979
“csureshkumargit” <csureshkumargce@gmail.com>
2022-01-03 22:12:08 -0500

    cookies for authentication

--- Controller/users.js
+++ Controller/users.js
@@ -62,8 +62,8 @@ exports.userLogin = async (req, res) => {
             return res.status(200).json({ message: "Password in correct", isAuthenticated: false });
 
         const token = jwt.sign({ userNameFound: emailFound.username }, process.env.CLIENT_KEY, { expiresIn: '1h' });
-        res.setHeader('set-cookie', `jwt_token=${token}; Path=/; maxAge:1000*60*60*1;SameSite=None; secure;httpOnly`);
-        res.status(200).send({ jwt: token, message: "You have been Logged in Successfully", username: emailFound.username, isAuthenticated: true });
+        //res.setHeader('set-cookie', `jwt_token=${token}; Path=/; maxAge:1000*60*60*1;SameSite=None; secure;httpOnly`);
+        res.status(200).header('set-cookie', `jwt_token=${token}; Path=/; maxAge=1000*60*60*1;SameSite=None; secure;httpOnly`).send({ jwt: token, message: "You have been Logged in Successfully", username: emailFound.username, isAuthenticated: true });
 
 
     }
